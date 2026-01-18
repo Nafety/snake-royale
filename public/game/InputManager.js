42e5a586ced4e3ca2f2b63b3000611dc783fc7e1
BARSOT Thomas <thomas.barsot@insa-rouen.fr>
2026-01-18 02:19:37 +0200

    inversion bloquée

--- public/game/InputManager.js
+++ public/game/InputManager.js
@@ -12,11 +12,11 @@ export class InputManager {
     if (this.cursors.right.isDown) dir = { x: 1, y: 0 };
     if (this.cursors.up.isDown) dir = { x: 0, y: -1 };
     if (this.cursors.down.isDown) dir = { x: 0, y: 1 };
-    
+
     // Interdire inversion immédiate (optionnel si tu as plusieurs segments)
-    // if (this.lastDirection.x + dir.x === 0 && this.lastDirection.y + dir.y === 0) {
-    //   dir = this.lastDirection;
-    // }
+     if (this.lastDirection.x + dir.x === 0 && this.lastDirection.y + dir.y === 0) {
+       dir = this.lastDirection;
+    }
 
     // Mettre à jour la direction actuelle
     if (dir.x !== 0 || dir.y !== 0) {
